a7fb70a49cae6a4249f676a2ccedcea4e8a917e0
gabrylo <gabrys-0000@wp.pl>
2023-10-27 10:48:04 +0200

    Dodaj pliki projektów.

---
+++ lab3gitv2/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace lab3gitv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxSize = new System.Windows.Forms.TextBox();
+            this.textBoxPercentage = new System.Windows.Forms.TextBox();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxSize
+            // 
+            this.textBoxSize.Location = new System.Drawing.Point(99, 103);
+            this.textBoxSize.Name = "textBoxSize";
+            this.textBoxSize.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSize.TabIndex = 1;
+            // 
+            // textBoxPercentage
+            // 
+            this.textBoxPercentage.Location = new System.Drawing.Point(99, 140);
+            this.textBoxPercentage.Name = "textBoxPercentage";
+            this.textBoxPercentage.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPercentage.TabIndex = 2;
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(99, 177);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumber.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.textBoxPercentage);
+            this.Controls.Add(this.textBoxSize);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxSize;
+        private System.Windows.Forms.TextBox textBoxPercentage;
+        private System.Windows.Forms.TextBox textBoxNumber;
+    }
+}
+
---
+++ lab3gitv2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3gitv2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double containerSize = Convert.ToDouble(textBoxSize.Text);
+            double alcoholPercentage = Convert.ToDouble(textBoxPercentage.Text);
+            int numberOfContainers = Convert.ToInt32(textBoxNumber.Text);
+
+        }
+    }
+}
